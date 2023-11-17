4c08b8185d867b48158715a9d5421436a7eeb435
ramlang <rachelelang7672@gmail.com>
2023-11-17 14:37:25 -0700

    Connecting phonebook backend to frontend

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 const app = express();
 
 const unknownEndpoint = (request, response) => {
@@ -10,6 +11,7 @@ morgan.token('body', req => {
   return JSON.stringify(req.body);
 })
 
+app.use(cors());
 app.use(express.json());
 app.use(morgan(
   ':method :url :status :res[content-length] - :response-time ms :body',
@@ -98,7 +100,7 @@ app.post('/api/persons/', (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
