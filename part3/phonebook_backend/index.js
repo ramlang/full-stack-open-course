415ecff5c53bda28ecdad52d4353e55c50121a4e
ramlang <rachelelang7672@gmail.com>
2023-11-17 19:06:01 -0700

    Connecting phonebook frontend to backend

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,106 +1,96 @@
 const express = require('express');
-const morgan = require('morgan');
 const cors = require('cors');
+const morgan = require('morgan');
 const app = express();
 
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-
-morgan.token('body', req => {
-  return JSON.stringify(req.body);
-})
-
-app.use(cors());
 app.use(express.json());
+app.use(express.static('dist'));
+app.use(cors());
+
+morgan.token('body', req => JSON.stringify(req.body));
 app.use(morgan(
   ':method :url :status :res[content-length] - :response-time ms :body',
-  { skip: function (req, res) { return req.method !== 'POST' } }
+  { skip: req => req.method !== 'POST' }
 ));
+
+const unknownEndpoint = (req, res) => {
+ res.status(404).send({ error: 'unknown endpoint' });
+}
 
 let persons = [
   { 
     "id": 1,
-    "name": "Arto Hellas", 
+    "name": "Teess", 
     "number": "040-123456"
   },
   { 
     "id": 2,
-    "name": "Ada Lovelace", 
+    "name": "Rachelee", 
     "number": "39-44-5323523"
   },
   { 
     "id": 3,
-    "name": "Dan Abramov", 
+    "name": "Alllen", 
     "number": "12-43-234345"
   },
   { 
     "id": 4,
-    "name": "Mary Poppendieck", 
+    "name": "Wesston", 
     "number": "39-23-6423122"
   }
 ]
 
-app.get('/api/persons/', (request, response) => {
-  response.json(persons);
-})
+// app.use(express.static('dist'))
 
-app.get('/api/persons/info', (request, response) => {
-  response.send(
-    `<p>Phonebook has info for ${persons.length} pepople.</p
-    <p>${new Date().toUTCString()}</p>`
-  );
-})
+app.get('/api/persons', (req, res) => res.json(persons));
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((p) => p.id === id);
+app.get('/api/persons/info', (req, res) => {
+  const timestamp = new Date().toUTCString();
+  const count = persons.length;
+  const msg = `<p>Phonebook has information for ${count} people.</p>` +
+    `<p>${timestamp}</p>`;
 
-  if (person) {
-    response.send(
-      `<p>Name: ${person.name}</p>
-      <p>Phone Number: ${person.number}</p>`
-    );
-  } else {
-    response.status(404).end();
-  }
-})
+  res.send(msg);
+});
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter(p => p.id !== id);
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((person) => person.id === id);
+  const msg = `<p>Name: ${person.name}</p><p>Phone: ${person.number}</p>`;
 
-  response.status(204).end()
-})
+  person ? res.send(msg) : res.status(404).end();
+});
 
-app.post('/api/persons/', (request, response) => {
-  const id = Math.floor(Math.random() * 1000);
-  const body = request.body;
-  const names = persons.map(person => person.name);
-
-  if (!body.name || !body.number) {
-    return response.status(400).json({
-      error: 'name and number missing',
-    })
-  } else if (names.includes(body.name)) {
-    return response.status(400).json({
-      error: 'name must be unique'
-    })
+app.post('/api/persons/', (req, res) => {
+  const id = Math.floor(Math.random() * 1_000);
+  const input = req.body;
+  const namesList = persons.map(person => person.name);
+  
+  if (!(input.name && input.number)) {
+    return res.status(400).json({ error: 'name and number missing' });
+  } else if (namesList.includes(input.name)) {
+    return res.status(400).json({ error: 'name must be unique' });
   }
 
-  const person = {
+  const newPerson = {
     id: id,
-    name: body.name,
-    number: body.number,
+    name: input.name,
+    number: input.number,
   }
 
-  persons = persons.concat(person);
-  response.json(person);
+  persons = persons.concat(newPerson);
+
+  res.json(newPerson);
 })
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter(person => person.id !== id);
+
+  res.status(204).end();
+});
 
 app.use(unknownEndpoint);
 
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+const PORT = process.env.PORT || 3002;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- part3/phonebook_frontend/src/App.jsx
+++ part3/phonebook_frontend/src/App.jsx
@@ -1,46 +1,47 @@
-import { useState, useEffect } from 'react'
-import personServices from './services/persons'
+import { useState, useEffect } from 'react';
+import personServices from './services/persons';
 
-const Persons = ({search, persons, onRemove}) => {
+const Person = ({name, number, handleRemove}) => {
+  return (
+    <li>
+      <p>Name: {name}</p>
+      <p>Phone: {number}</p>
+      <button onClick={handleRemove}>Remove</button>
+    </li>
+  )
+}
+
+const Persons = ({persons, handleRemove}) => {
   return (
     <ul>
       {persons.map(person => {
-        if (person.name.toLowerCase().slice(0, search.length) === search.toLowerCase()) {
-          return <Person
-            key={person.id}
-            name={person.name}
-            number={person.number}
-            onRemove={() => onRemove(person.id)}
-          />
-        }
+        return <Person
+          key={person.id}
+          name={person.name}
+          number={person.number}
+          handleRemove={() => handleRemove(person.id)}
+        />
       })}
     </ul>
   )
 }
 
-const Person = ({name, number, onRemove}) => {
-  return (
-    <li>
-      <p>Name: {name}</p>
-      <p>Phone: {number}</p>
-      <button onClick={onRemove}>Remove</button>
-    </li>
-  )
-}
-
 const PersonForm = (props) => {
   return (
-    <form onSubmit={props.onSubmit}>
+    <form onSubmit={props.handleSubmit}>
       <div>
-        Name: <input 
-          value={props.newName}
-          onChange={props.onChangeName}/>
+        Name:
+        <input
+          value={props.name}
+          onChange={props.handleName}
+        />
       </div>
       <div>
-        Phone Number: <input
-          value={props.newNumber}
-          onChange={props.onChangeNumber}
-        />
+        Number:
+          <input
+            value={props.number}
+            onChange={props.handleNumber}
+          />
       </div>
       <div>
         <button type="submit">Add</button>
@@ -49,113 +50,120 @@ const PersonForm = (props) => {
   )
 }
 
-const Filter = (props) => {
+const Search = (newSearch, handleSearch) => {
   return (
     <input 
-      onChange={props.onChangeSearch}
-      value={props.newSearch}
+      value={newSearch}
+      onChange={handleSearch}
     />
   )
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([])
-  const [newName, setNewName] = useState('')
+  const [persons, setPersons] = useState([]);
+  const [matches, setMatches] = useState([]);
+  const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
-  const [newSearch, setSearch] = useState('');
+  const [search, setSearch] = useState('');
+
 
   useEffect(() => {
     personServices.getAll()
-      .then((initialPersons) => setPersons(initialPersons));
+      .then(initialPersons => {
+        setPersons(initialPersons);
+      });
   }, []);
 
-  const onSubmit = (event) => {
+  const findPersonByName = (name) => {
+    return persons.find(person => {
+      return person.name.toLowerCase() === name.toLowerCase();
+    })
+  }
+
+  const updatePersonById = (id, newPerson) => {
+    return persons.map(person => person.id !== id ? person : newPerson);
+  }
+  
+  const handleSubmit = (event) => {
     event.preventDefault();
-    const names = persons.map(person => person.name);
-    let newPerson = {name: newName, number: newNumber};
-    
-    if (names.includes(newName)) {
-      const person = persons.find((elem) => {
-        return elem.name.toLowerCase() === newName.toLowerCase()
-      });
-      const msg = `Do you want to overwrite ${person.name}'s current number?`;
+    const person = findPersonByName(newName);
 
+    if (person) {
+      const msg = `Do you want to update ${person.name}'s current number?`;
+      
       if (window.confirm(msg)) {
-        const newInfo = {...person, number: newPerson.number};
+        const id = person.id;
 
-        personServices.update(person.id, newInfo)
+        personServices.update(id, { ...person, number: newNumber })
           .then(returnedPerson => {
-            setPersons(persons.map(person => {
-              return person.id !== returnedPerson.id ? person : returnedPerson
-            }));
+            const updatedList = updatePersonById(id, returnedPerson);
+    
+            setPersons(updatedList);
             setNewName('');
             setNewNumber('');
           })
       }
     } else {
-      personServices.add(newPerson)
+      personServices.add({ name: newName, number: newNumber })
         .then((addedPerson) => {
-          setPersons(persons.concat(addedPerson));
+          const updatedList = persons.concat(addedPerson);
+
+          setPersons(updatedList);
           setNewName('');
           setNewNumber('');
         });
     }
   }
 
-  const onRemove = (id) => {
-    const person = persons.find((elem) => elem.id === id);
-    if (window.confirm(`Do you want to delete ${person.name}?`)) {
-      personServices.remove(id);
-      setPersons(persons.filter(person => person.id !== id))
-    }
-  }
-
-  // noteService
-  // .update(id, changedNote)
-  // .then(returnedNote => {
-  //   setNotes(notes.map(note => note.id !== id ? note : returnedNote))
-  // })
-
-  const onUpdate = (id, number) => {
-    // const person = persons.find((elem) => elem.id === id);
-    // if (window.confirm(`Do you want to overwrite ${person.name}'s current number?`)) {
-    //   personServices.update(id, {...person, number: number});
-    //   setPersons(persons.filter(person => person.id !== id))
-    //   setPersons(persons.concat({...person, number: number}))
-    // }
-  }
-
-  const onChangeName = (event) => {
+  const handleName = (event) => {
     setNewName(event.target.value);
   }
 
-  const onChangeNumber = (event) => {
+  const handleNumber = (event) => {
     setNewNumber(event.target.value);
   }
 
-  const onChangeSearch = (event) => {
-    setSearch(event.target.value);
+  const handleRemove = (id) => {
+    const person = persons.find(person => person.id === id);
+    const msg = `Are you sure you want to delete ${person.name}?`;
+
+    if (window.confirm(msg)) {
+      personServices.remove(id);
+      setPersons(persons.filter(person => person.id !== id));
+    }
+  }
+
+  const handleSearch = (event) => {
+    setSearch(event.target.value.toLowerCase());
+
+    const filteredPersons = persons.filter(person => {
+      const name = person.name.toLowerCase();
+      return search === name.slice(0, search.length);
+    });
+
+    setMatches(search === '' ? persons : filteredPersons);
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <Filter 
-        onChangeSearch={onChangeSearch}
+      <Search 
         newSearch={newSearch}
+        handleSearch={handleSearch}
       />
       <h3>Add a New Contact</h3>
       <PersonForm
-        newName={newName}
-        newNumber={newNumber}
-        onChangeName={onChangeName}
-        onChangeNumber={onChangeNumber}
-        onSubmit={onSubmit}
+        name={newName}
+        number={newNumber}
+        handleName={handleName}
+        handleNumber={handleNumber}
+        handleSumit={handleSubmit}
       />
       <h3>Numbers</h3>
-      <Persons search={newSearch} persons={persons} onRemove={onRemove}/>
+      <Persons persons={matches} handleRemove={handleRemove}/>
     </div>
   )
+
 }
 
 export default App
